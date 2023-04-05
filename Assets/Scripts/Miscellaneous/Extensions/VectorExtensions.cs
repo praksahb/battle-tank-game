84a4d64d595d9242cbe6745a2eaabc5f85800682
praksahb <39479839+praksahb@users.noreply.github.com>
2023-04-05 21:42:09 +0530

    field of view added into enemyTankView -- stashing change
    using reference/guide/copying-code from https://www.youtube.com/watch?v=rQG9aUWarwE
    
    will have to see what is actually happening later after dinner.

--- Assets/Scripts/Miscellaneous/Extensions/VectorExtensions.cs
+++ Assets/Scripts/Miscellaneous/Extensions/VectorExtensions.cs
@@ -1,3 +1,4 @@
+using TankBattle.Tank.EnemyTank;
 using UnityEngine;
 
 namespace TankBattle.Extensions
@@ -7,6 +8,15 @@ namespace TankBattle.Extensions
         public static Vector3 switchYAndZValues(this Vector2 vector)
         {
             return new Vector3(vector.x, 0, vector.y);
+        }
+
+        public static Vector3 DirFromAngle(this EnemyTankView enemyObj, float angleInDegrees, bool angleIsGlobal)
+        {
+            if (!angleIsGlobal)
+            {
+                angleInDegrees += enemyObj.transform.eulerAngles.y;
+            }
+            return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
         }
     }
 }
--- Assets/Scripts/TankService/EnemyTank/EnemyTankView.cs
+++ Assets/Scripts/TankService/EnemyTank/EnemyTankView.cs
@@ -1,5 +1,9 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 using UnityEngine.UI;
+using TankBattle.Extensions;
+using System.Collections.Generic;
+using System.Collections;
 
 namespace TankBattle.Tank.EnemyTank
 {
@@ -20,7 +24,38 @@ namespace TankBattle.Tank.EnemyTank
         [SerializeField]
         public TankAttackingState attackingState;
 
+        public float viewRadius;
+        [Range(0, 360)]
+        public float viewAngle;
+
+        public LayerMask targetMask;
+        public LayerMask obstacleMask;
+
+        public List<Transform> visibleTargets = new List<Transform>();
+
         public EnemyStateController enemyStateController;
+
+        private Coroutine searchPlayerCoroutine;
+
+        private void Start()
+        {
+            ChangeState(startingState);
+
+            if(searchPlayerCoroutine != null)
+            {
+                StopCoroutine(searchPlayerCoroutine);
+            }
+            searchPlayerCoroutine = StartCoroutine(FindTargetsWithDelay(0.2f));
+        }
+
+        private IEnumerator FindTargetsWithDelay(float delay)
+        {
+            while(true)
+            {
+                yield return new WaitForSeconds(delay);
+                FindVisibleTargets();
+            }
+        }
 
         public TankState GetCurrentState()
         {
@@ -45,9 +80,46 @@ namespace TankBattle.Tank.EnemyTank
             currentState.OnEnterState();
         }
 
-        private void Start()
+        private void FindVisibleTargets()
         {
-            ChangeState(startingState);
+            Collider[] targetsinViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+
+            for(int i =0; i < targetsinViewRadius.Length; i++)
+            {
+                visibleTargets.Clear();
+                Transform target = targetsinViewRadius[i].transform;
+                Vector3 dirToTarget = (target.position - transform.position).normalized;
+                if(Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+                {
+                    float distToTarget = Vector3.Distance(transform.position, target.position);
+                    if(!Physics.Raycast(transform.position, dirToTarget, distToTarget, obstacleMask))
+                    {
+                        // target is in view range of enemy -- can use list to store if more than one targets
+                        visibleTargets.Add(target);
+                    }
+                }
+            }
+        }
+    }
+
+    [CustomEditor (typeof (EnemyTankView))]
+    public class FieldOfViewEditor : Editor
+    {
+        private void OnSceneGUI()
+        {
+            EnemyTankView enemyFOV = (EnemyTankView)target;
+            Handles.color = Color.white;
+            Vector3 viewAngleA = enemyFOV.DirFromAngle(-enemyFOV.viewAngle / 2, false);
+            Vector3 viewAngleB = enemyFOV.DirFromAngle(enemyFOV.viewAngle / 2, false);
+            Handles.DrawWireArc(enemyFOV.transform.position, Vector3.up, viewAngleA, enemyFOV.viewAngle, enemyFOV.viewRadius);
+            Handles.DrawLine (enemyFOV.transform.position,enemyFOV.transform.position + viewAngleA * enemyFOV.viewRadius);
+            Handles.DrawLine(enemyFOV.transform.position, enemyFOV.transform.position + viewAngleB * enemyFOV.viewRadius);
+
+            Handles.color = Color.red;
+            for(int i = 0; i < enemyFOV.visibleTargets.Count; i++)
+            {
+                Handles.DrawLine(enemyFOV.transform.position, enemyFOV.visibleTargets[i].transform.position);
+            }
         }
     }
 }
