f07155e484702660d6edda97c5524f8a77004a4d
praksahb <39479839+praksahb@users.noreply.github.com>
2023-04-08 22:33:04 +0530

    projectile line working simulating almost accurate bullet trajectory.

---
+++ Assets/Scripts/TankService/PlayerTank/BulletProjectile.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using TankBattle.Tank;
+using TankBattle.Tank.PlayerTank;
+using UnityEngine;
+
+namespace TankBattle.Services
+{
+    public class BulletProjectile : MonoBehaviour
+    {
+        [SerializeField] private LineRenderer trajectoryLineRenderer;
+        [SerializeField] private int numberOfPoints = 30;
+
+        private float currentLaunchForce;
+
+        private TankController playerController;
+        private Transform firePointTransform;
+
+        private List<Vector3> trajectoryPoints = new List<Vector3>();
+
+        private float timeStep = 0.05f;
+        private float checkCollisionRadius = 0.15f;
+
+        private void Update()
+        {
+
+            if (trajectoryLineRenderer != null)
+            {
+                DrawTrajectory();
+            }
+        }
+
+        private void Start()
+        {
+            playerController = PlayerService.Instance.GetTankController();
+            currentLaunchForce = playerController.TankModel.MinLaunchForce;
+        }
+
+        private void DrawTrajectory()
+        {
+            firePointTransform = playerController.TankView.GetFireTransform();
+
+            float gravity = Physics.gravity.y;
+
+            trajectoryPoints.Clear();
+            trajectoryPoints.Add(firePointTransform.position);
+
+            Vector3 initialVelocity = firePointTransform.forward * currentLaunchForce;
+            Vector3 position = firePointTransform.position;
+            Vector3 currentVelocity = initialVelocity;
+
+            for (int i = 1; i < numberOfPoints; i++)
+            {
+                position += currentVelocity * timeStep + 0.5f * Vector3.up * gravity * timeStep * timeStep;
+                currentVelocity += firePointTransform.up * gravity * timeStep;
+
+                if(CheckForCollision(position))
+                {
+                    break;
+                }
+
+                trajectoryPoints.Add(position);
+            }
+            trajectoryLineRenderer.positionCount = trajectoryPoints.Count;
+            trajectoryLineRenderer.SetPositions(trajectoryPoints.ToArray());
+        }
+
+        private bool CheckForCollision(Vector3 position)
+        {
+            Collider[] hitColliders = new Collider[5];
+            int hits = Physics.OverlapSphereNonAlloc(position, checkCollisionRadius, hitColliders);
+            if(hits > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
